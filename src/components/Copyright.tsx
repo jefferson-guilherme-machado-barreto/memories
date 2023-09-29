d9dcf88d293d0bd63e9a945d0ea8e068fff83210
Guilherme Machado <guilhermemachado.jeff@gmail.com>
2023-09-29 12:53:32 -0300

    create project

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,10 @@
+export default function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da {' '}
+      <a target="_blank" rel="noreferrer" className="underlinde hover:text-gray-100" href="https://rocketseat.com.br">
+        Rocketseat
+      </a>
+    </div>
+  )
+}
